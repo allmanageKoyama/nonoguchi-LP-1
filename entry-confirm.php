a6e77a20320ad016a821e8263a7a825fbb897761
allmanageKoyama <y-koayama@a-manage.jp>
2023-04-10 15:54:24 +0900

    デモ用フォーム追加

---
+++ entry-confirm.php
@@ -0,0 +1,272 @@
+<!doctype html>
+
+<html lang="ja">
+
+<head prefix="og: https://ogp.me/ns#">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2, user-scalable=1" />
+  <meta name="format-detection" content="telephone=no">
+
+  <title>採用情報 | 野々口株式会社</title>
+  <meta name="description" content="「採用情報」ページです。野々口株式会社は、御客様のニーズ、時代の流れに対応出来るよう、体制を調えております。どのポジション（生地・製品）でも最適な提案を致します。">
+  <link rel="canonical" href="https://www.nonoguchi.net/recruit/">
+  <link rel="apple-touch-icon" sizes="180x180" href="https://www.nonoguchi.net/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="https://www.nonoguchi.net/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="https://www.nonoguchi.net/favicon/favicon-16x16.png">
+  <link rel="manifest" href="https://www.nonoguchi.net/favicon/site.webmanifest">
+  <link rel="mask-icon" href="https://www.nonoguchi.net/favicon/safari-pinned-tab.svg" color="#040474">
+  <meta name="msapplication-TileColor" content="#2b5797">
+  <meta name="theme-color" content="#ffffff">
+
+  <meta property="og:title" content="採用情報 | 野々口株式会社">
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="「採用情報」ページです。野々口株式会社は、御客様のニーズ、時代の流れに対応出来るよう、体制を調えております。どのポジション（生地・製品）でも最適な提案を致します。">
+  <meta property="og:url" content="https://www.nonoguchi.net/recruit/">
+  <meta property="og:site_name" content="野々口株式会社 - 白衣・作業着・スポーツウェア・カジュアル・帽子・資材等">
+  <meta property="og:image" content="https://web-checker3.net/nonoguchi-recruit/demo/src/img/ogp.png">
+
+  <script>
+    (function(d) {
+      var config = {
+          kitId: 'lsw4czv',
+          scriptTimeout: 3000,
+          async: true
+        },
+        h = d.documentElement,
+        t = setTimeout(function() {
+          h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
+        }, config.scriptTimeout),
+        tk = d.createElement("script"),
+        f = false,
+        s = d.getElementsByTagName("script")[0],
+        a;
+      h.className += " wf-loading";
+      tk.src = 'https://use.typekit.net/' + config.kitId + '.js';
+      tk.async = true;
+      tk.onload = tk.onreadystatechange = function() {
+        a = this.readyState;
+        if (f || a && a != "complete" && a != "loaded") return;
+        f = true;
+        clearTimeout(t);
+        try {
+          Typekit.load(config)
+        } catch (e) {}
+      };
+      s.parentNode.insertBefore(tk, s)
+    })(document);
+  </script>
+  <link href="//cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/css/drawer.min.css" rel="stylesheet">
+  <link href="https://www.nonoguchi.net//CSS/initialize.min.css" rel="stylesheet" type="text/css">
+  <link href="https://www.nonoguchi.net//CSS/common.css" rel="stylesheet" type="text/css">
+  <link href="https://www.nonoguchi.net//CSS/page.css" rel="stylesheet" type="text/css">
+  <link href="https://www.nonoguchi.net//CSS/page_sp.css" rel="stylesheet" type="text/css">
+  <link href="src/css/recruit.css?css=11" rel="stylesheet" type="text/css">
+
+  <link rel='dns-prefetch' href='//s.w.org' />
+  <link rel='stylesheet' id='wp-pagenavi-css' href='https://www.nonoguchi.net/wp-content/plugins/wp-pagenavi/pagenavi-css.css' type='text/css' media='all' />
+  <script src='https://www.nonoguchi.net/wp-includes/js/jquery/jquery.js'></script>
+  <script src='https://www.nonoguchi.net/wp-includes/js/jquery/jquery-migrate.min.js'></script>
+</head>
+
+<body id="recruit" class="under">
+  <div class="drawer drawer--right">
+    <div id="wrapper">
+      <header class="header">
+        <div class="inner">
+          <div class="logo_area">
+            <div class="hdr_logo"><a href="https://www.nonoguchi.net/"><img src="src/img/h_logo.svg" alt="野々口株式会社"></a></div>
+          </div>
+          <div class="r_area">
+            <nav id="gNavi" class="visible_pc">
+              <ul class="navi">
+                <li class="nav-item"><a href="#section-01"><span>メッセージ</span></a></li>
+                <li class="nav-item"><a href="#section-02"><span>求める人物像</span></a></li>
+                <li class="nav-item"><a href="#section-03"><span>野々口の人々</span></a></li>
+                <li class="nav-item"><a href="#section-04"><span>ワークライフ</span></a></li>
+                <li class="nav-item"><a href="#section-05"><span>社内風景</span></a></li>
+                <li class="nav-item"><a href="#section-06"><span>募集要項</span></a></li>
+                <li class="nav-item"><a href="#entry"><span>ENTRY</span></a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+      <div class="hdr_btn visible_tb">
+        <button type="button" class="drawer-toggle drawer-hamburger"><span class="drawer-hamburger-icon"></span></button>
+      </div>
+      <nav class="drawer-nav">
+        <ul class="drawer-menu main_menu">
+          <li class="nav-item"><a href="#section-01">
+              <p class="en">MESSAGE</p><span>メッセージ</span>
+            </a></li>
+          <li class="nav-item"><a href="#section-02">
+              <p class="en">IDEAL CANDIDATE</p><span>求める人物像</span>
+            </a></li>
+          <li class="nav-item"><a href="#section-03">
+              <p class="en">MEMBER</p><span>野々口の人々</span>
+            </a></li>
+          <li class="nav-item"><a href="#section-04">
+              <p class="en">WORK LIFE BALANCE</p><span>ワークライフ</span>
+            </a></li>
+          <li class="nav-item"><a href="#section-05">
+              <p class="en">COMPANY SCENERY</p><span>社内風景</span>
+            </a></li>
+          <li class="nav-item"><a href="#section-06">
+              <p class="en">REQUIREMENTS</p><span>募集要項</span>
+            </a></li>
+
+          <ul class="sub_menu">
+            <li class="nav-item"><a href="#entry"><span>ENTRY</span></a></li>
+          </ul>
+          </li>
+        </ul>
+      </nav>
+      <div id="contents" class="cf">
+        <div id="main-cts" class="cf">
+       
+          <section class="section-form">
+            <span id="entry"></span>
+            <div class="container">
+            <div class="ttl_wrap">
+              <h2 class="ttl_01 is-set">ENTRY CONFIRM</h2>
+              <p class="lead is-set">応募内容の確認</p>
+            </div>
+            <p class="text">以下の内容で送信します</p>
+              <table class="cform">
+                <tbody>
+                  <tr>
+                    <th>名前 <span class="required-srt">必須</span></th>
+                    <td class="demo">[mwform_text name="お名前" size="60" placeholder="名前"]</td>
+                    <td>名前</td>
+                  </tr>
+
+
+                  <tr>
+                    <th>電話番号</th>
+                    <td class="demo"><label for="tel">
+                      </label>[mwform_text name="電話" size="60" placeholder="電話番号"]
+                    </td>
+                    <td>電話番号</td>
+                  </tr>
+                  <tr>
+                    <th>メールアドレス <span class="required-srt">必須</span></th>
+                    <td class="demo">[mwform_email name="メールアドレス" size="60" placeholder="メールアドレス"]</td>
+                    <td>メールアドレス</td>
+                  </tr>
+
+                  <tr>
+                    <th>履歴書</th>
+                    <td class="demo">[mwform_file name="ファイル"]</td>
+                    <td><input type="file" name="メールアドレス" id=""></td>
+                  </tr>
+
+                </tbody>
+              </table>
+              <form action="entry-thanks.php">
+              <div class="form-btn_area">
+                
+                <div class="submit-btn demo">[mwform_submitButton name="確認送信" confirm_value="確認画面へ進む" submit_value="送信する"]</div>
+                <div class="submit-btn"><input type="submit" value="送信する"></div>
+                <p class="back-btn demo" style="text-align: center;">[mwform_backButton value="戻る"]</p>
+                <p class="back-btn" style="text-align: center;"><input type="button" value="戻る"></p>
+              </div>
+              </form>
+            </div>
+          </section>
+        </div>
+      </div>
+
+
+
+      <footer id="footer">
+        <div class="top_box">
+          <div class="sec_inner">
+            <div class="l_box">
+              <img src="https://www.nonoguchi.net/Image/common/f_logo.png" alt="野々口株式会社">
+              <p>〒541-0057 大阪市中央区北久宝寺町1-2-15</p>
+              <p>TEL：06-6264-1161</p>
+            </div>
+            <ul class="r_box">
+              <li><a href="https://www.nonoguchi.net/">ホーム</a></li>
+              <li><a href="https://www.nonoguchi.net/company/">会社案内</a></li>
+              <li><a href="https://www.nonoguchi.net/business/">事業案内</a></li>
+              <li><a href="https://www.nonoguchi.net/products/">製品案内</a></li>
+              <li><a href="https://www.nonoguchi.net/recruit/">採用情報</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="bottom_box">
+          <div class="sec_inner">
+            <p>Copyright © 野々口株式会社 All Rights Reserved.</p>
+            <p><a href="https://www.nonoguchi.net/privacy/">プライバシーポリシー</a></p>
+          </div>
+        </div>
+      </footer>
+      <div id="page_top" class="en"><a href="#"></a></div>
+
+      <!-- JavaScript -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/iScroll/5.2.0/iscroll.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/js/drawer.min.js"></script>
+      <script src="https://www.nonoguchi.net//JS/common.js"></script>
+      <script src="https://www.nonoguchi.net//JS/ofi.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inview/1.0.0/jquery.inview.min.js"></script>
+
+      <script>
+        $(window).on("load", function() {
+          $('body').addClass('mv-loaded')
+        });
+        $(".is-set").on("inview", function(event, isInView) {
+          if (isInView) {
+            $(this).addClass("is-action");
+          } else {
+            // $(this).removeClass("is-action");
+            // $(this).removeClass("is-detail");
+            // $('.message-box').removeClass('set')
+            // $('.set').removeAttr('style')
+            // $('.pnl').removeAttr('style')
+
+          }
+        });
+
+        $(".move").on("inview", function(event, isInView) {
+          if (isInView) {
+            $(this).addClass("on");
+          } else {
+            $(this).removeClass("on");
+          }
+        });
+        $('.section-03 .contetnts .pnl').on('click', function() {
+          if ($(this).hasClass('is-select')) {
+            $('.section-03 .contetnts').removeClass('is-detail')
+            $(this).removeClass('is-select')
+            $(this).removeAttr('style')
+            $('.message-box').removeClass('set')
+          } else {
+            $('.section-03 .contetnts').addClass('is-detail')
+            $(this).css('z-index', '100')
+            $(this).addClass('is-select')
+            var box = $(this).attr('data-pnl_no')
+            $('#person-' + box).addClass('set')
+          }
+        })
+        $('.message-box .btn').on('click', function() {
+          $('.section-03 .contetnts').removeClass('is-detail')
+          $('.message-box').removeClass('set')
+          $('.section-03 .contetnts .pnl').removeClass('is-select')
+          $('.section-03 .contetnts .pnl').removeAttr('style')
+        })
+        $(".section-06").on("inview", function(event, isInView) {
+          if (isInView) {
+            $('.section-07 .is-set').removeClass("is-action");
+          } else {
+            $('.section-07 .is-set').addClass("is-action");
+          }
+        });
+      </script>
+    </div>
+  </div>
+</body>
+
+</html>
